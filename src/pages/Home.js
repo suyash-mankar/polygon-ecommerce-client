41e9745e56e1d5b03b1369ba95968e24d85b4e2e
Suyash Mankar <suyashmankar@gmail.com>
2023-01-28 01:03:25 +0530

    setup search filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,9 @@ import { useParams } from "react-router";
 import { addItemsToCart } from "../actions/cartAction";
 
 function Home() {
+  // const [searchText, setSearchText] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const dispatch = useDispatch();
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
@@ -30,55 +33,93 @@ function Home() {
     toast.success("Item added to cart");
   };
 
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  const handleSearchFilter = (e) => {
+    const searchWord = e.target.value;
+    if (searchWord === "") {
+      setFilteredProducts(products);
+    }
+    const searchedProducts = products.filter((product) => {
+      return product.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setFilteredProducts(searchedProducts);
+  };
+
   return (
     <div className="home_container">
       {loading ? (
         <Loader />
       ) : (
-        <div className="all_products_container">
-          {products &&
-            products.map((product) => {
-              return (
-                <Link
-                  to={`/products/${product._id}`}
-                  key={product._id}
-                  className="product_card"
-                >
-                  <img
-                    src={product.image.url}
-                    alt="product"
-                    className="product_img"
-                  />
+        <>
+          <div className="filters_container">
+            <h3>Filters:</h3>
+            <div className="search_filter">
+              <p>search by Product Title:</p>
+              <input
+                type="text"
+                onChange={handleSearchFilter}
+                placeholder="Enter Product Title"
+              />
+            </div>
+          </div>
 
-                  <div className="product_details">
-                    <p className="collection_name">
-                      {product.collectionName}
-                      <img
-                        src="https://cdn-icons-png.flaticon.com/512/7641/7641727.png"
-                        alt="verified-icon"
-                        style={{ width: "20px", marginLeft: "5px" }}
-                      />
-                    </p>
-                    <p className="product_title">{product.title}</p>
-                    <p className="product_price">
-                      <span>Price:</span> ${product.price}
-                    </p>
-                    <p className="product_chain">
-                      <span>Chain:</span> {product.chain}
-                    </p>
-                    <Button
-                      variant="primary"
-                      onClick={() => {
-                        addToCartHandler(product._id);
-                      }}
-                    >
-                      Add to cart
-                    </Button>
-                  </div>
-                </Link>
-              );
-            })}
-        </div>
+          <div
+            className="all_products_container"
+            style={filteredProducts.length === 0 ? { height: "100vh" } : null}
+          >
+            {filteredProducts.length === 0 ? (
+              <h1 style={{ color: "white" }}> No Products Found</h1>
+            ) : (
+              <>
+                {products &&
+                  filteredProducts.map((product) => {
+                    return (
+                      <Link
+                        to={`/products/${product._id}`}
+                        key={product._id}
+                        className="product_card"
+                      >
+                        <img
+                          src={product.image.url}
+                          alt="product"
+                          className="product_img"
+                        />
+
+                        <div className="product_details">
+                          <p className="collection_name">
+                            {product.collectionName}
+                            <img
+                              src="https://cdn-icons-png.flaticon.com/512/7641/7641727.png"
+                              alt="verified-icon"
+                              style={{ width: "20px", marginLeft: "5px" }}
+                            />
+                          </p>
+                          <p className="product_title">{product.title}</p>
+                          <p className="product_price">
+                            <span>Price:</span> ${product.price}
+                          </p>
+                          <p className="product_chain">
+                            <span>Chain:</span> {product.chain}
+                          </p>
+                          <Button
+                            variant="primary"
+                            onClick={() => {
+                              addToCartHandler(product._id);
+                            }}
+                          >
+                            Add to cart
+                          </Button>
+                        </div>
+                      </Link>
+                    );
+                  })}
+              </>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
